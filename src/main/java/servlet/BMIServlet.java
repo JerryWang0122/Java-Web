d4b504ea5a287bc884dc8ed839b116d9f3d378a6
JerryWang0122 <school832@gmail.com>
2024-04-26 13:48:55 +0800

    feat: complete bmi calculate

---
+++ src/main/java/servlet/BMIServlet.java
@@ -0,0 +1,36 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/servlet/bmi")
+public class BMIServlet extends HttpServlet {
+    // TODO: 上午最後
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 編碼
+        resp.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=utf-8");
+
+        // 接收參數
+        String userName = req.getParameter("userName");
+        String userHeight = req.getParameter("userHeight");
+        String userWeight = req.getParameter("userWeight");
+
+        // 計算 BMI
+        // 將 userHeight & userWeight 由 String 轉 Double
+        double height = Double.parseDouble(userHeight);
+        double weight = Double.parseDouble(userWeight);
+        double bmi = weight / Math.pow(height / 100, 2);
+
+        // 回應內容
+        resp.getWriter().print("BMI Result = " + bmi);
+    }
+
+
+}

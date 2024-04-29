3325d0d1619fb4d3e89666ccea344938eeb73c62
JerryWang0122 <school832@gmail.com>
2024-04-29 13:46:54 +0800

    feat: In-Memory DAO

---
+++ src/main/java/guestbook/dao/GuestBookDaoInMemory.java
@@ -0,0 +1,50 @@
+package guestbook.dao;
+
+import guestbook.model.GuestBook;
+
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class GuestBookDaoInMemory implements GuestBookDao{
+    // In-Memory 資料
+    private List<GuestBook> guestBooks = new CopyOnWriteArrayList<>();
+
+    @Override
+    public int create(GuestBook guestBook) {
+        guestBooks.add(guestBook);
+        return 1;
+    }
+
+    @Override
+    public GuestBook findById(Integer id) {
+        return guestBooks.stream().filter(gb -> gb.getId().equals(id)).findFirst().orElseGet(null);
+    }
+
+    @Override
+    public List<GuestBook> findAll() {
+        return guestBooks;
+    }
+
+    @Override
+    public int update(GuestBook guestBook) {
+        GuestBook guestBookUpt = findById(guestBook.getId());
+        if (guestBookUpt == null) {
+            return 0;
+        }
+        guestBookUpt.setUserName(guestBook.getUserName());
+        guestBookUpt.setContent(guestBook.getContent());
+        guestBookUpt.setUpdateDate(new Date());
+        return 1;
+    }
+
+    @Override
+    public int deleteById(Integer id) {
+        GuestBook guestBookDel = findById(id);
+        if (guestBookDel == null) {
+            return 0;
+        }
+        guestBooks.remove(guestBookDel);
+        return 1;
+    }
+}

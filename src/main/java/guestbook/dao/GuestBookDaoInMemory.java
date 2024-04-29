627ea43c4248a96c82db3253b2ef1301613398e7
JerryWang0122 <school832@gmail.com>
2024-04-29 14:50:25 +0800

    feat: DAO In-Memory 修正

--- src/main/java/guestbook/dao/GuestBookDaoInMemory.java
+++ src/main/java/guestbook/dao/GuestBookDaoInMemory.java
@@ -19,6 +19,7 @@ public class GuestBookDaoInMemory implements GuestBookDao{
                         .orElse(0);
         maxId += 1;  // 將最大值加一
         guestBook.setId(maxId);
+        guestBook.setCreateDate(new Date());
         guestBooks.add(guestBook);
         return 1;
     }

443805a87b122e23609a356f629542a620433dd0
JerryWang0122 <school832@gmail.com>
2024-04-29 12:24:26 +0800

    feat: GuestBook class

--- src/main/java/guestbook/model/GuestBook.java
+++ src/main/java/guestbook/model/GuestBook.java
@@ -1,10 +1,21 @@
 package guestbook.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
+@Data
+@NoArgsConstructor
 public class GuestBook {
     private Integer id;
-    private String name;
+    private String userName;
     private String content;
     private Date createDate;
+    private Date updateDate;
+
+    public GuestBook(String userName, String content) {
+        this.userName = userName;
+        this.content = content;
+    }
 }

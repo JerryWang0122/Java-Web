2237e0bd3e0310eaa3f99ab8f8e97a60e165dec0
JerryWang0122 <school832@gmail.com>
2024-04-29 14:20:50 +0800

    feat: GuestBookServiceImpl finish

---
+++ src/main/java/guestbook/service/GuestBookServiceImpl.java
@@ -0,0 +1,56 @@
+package guestbook.service;
+
+import guestbook.dao.GuestBookDao;
+import guestbook.dao.GuestBookDaoInMemory;
+import guestbook.model.GuestBook;
+
+import java.util.List;
+
+public class GuestBookServiceImpl implements GuestBookService{
+
+    private GuestBookDao dao = new GuestBookDaoInMemory();
+    @Override
+    public boolean add(String userName, String content) {
+        GuestBook gb = new GuestBook(userName, content);
+        int rowcount = dao.create(gb);
+        return rowcount == 1;
+    }
+
+    @Override
+    public GuestBook getById(Integer id) {
+        return dao.findById(id);
+    }
+
+    @Override
+    public List<GuestBook> queryAll() {
+        return dao.findAll();
+    }
+
+    @Override
+    public boolean updateUsername(Integer id, String username) {
+        GuestBook gb = dao.findById(id);
+        if (gb == null) {
+            return false;
+        }
+        gb.setUserName(username);
+        int rowcount = dao.update(gb);
+        return rowcount == 1;
+    }
+
+    @Override
+    public boolean updateContent(Integer id, String content) {
+        GuestBook gb = dao.findById(id);
+        if (gb == null) {
+            return false;
+        }
+        gb.setContent(content);
+        int rowcount = dao.update(gb);
+        return rowcount == 1;
+    }
+
+    @Override
+    public boolean removeById(Integer id) {
+        int rowcount = dao.deleteById(id);
+        return rowcount == 1;
+    }
+}

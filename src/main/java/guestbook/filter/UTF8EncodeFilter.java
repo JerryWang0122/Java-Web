f5b48a2a220fb83a59bed659af062f12e36eae74
JerryWang0122 <school832@gmail.com>
2024-05-03 10:53:34 +0800

    feat: 使用filter完成編碼

--- src/main/java/guestbook/filter/UTF8EncodeFilter.java
+++ src/main/java/guestbook/filter/UTF8EncodeFilter.java
@@ -9,10 +9,20 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+// 過濾/攔截
 @WebFilter("/guestbook/*")
-public class StopFilter extends HttpFilter {
+public class UTF8EncodeFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
-        res.getWriter().print("Stop!");
+        // 這層 Filter 做的事 (Middle ware)
+        // 編碼
+        req.setCharacterEncoding("UTF-8");
+        res.setCharacterEncoding("UTF-8");
+        res.setContentType("text/html;charset=UTF-8");
+
+        // res.getWriter().print("UTF8Encode");
+
+        // 往下繼續執行
+        chain.doFilter(req, res);
     }
 }
